7ad55057a02afdb8b80fc6a172a66f068fdd653a
ViktorPank <41527815+ViktorPank@users.noreply.github.com>
2020-07-22 12:12:08 +0400

    add FXML and icons

--- javaFX_app/src/main/java/JavaFXTest.java
+++ javaFX_app/src/main/java/JavaFXTest.java
@@ -1,6 +1,41 @@
-public class JavaFXTest {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
+import java.io.InputStream;
+
+public class JavaFXTest extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //Load FXML and set setting
+        try {
+            // Read file fxml and draw interface.
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("/MyScene.fxml"));
+            primaryStage.setTitle("My Application");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /*
+        Load icon
+         */
+        InputStream iconStream = getClass().getResourceAsStream("/images/man.png");
+        Image image = new Image(iconStream);
+        primaryStage.getIcons().add(image);
+
+       // primaryStage.show();
 
     }
 }
