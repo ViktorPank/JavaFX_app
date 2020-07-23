96c4d4e4694d042aa560087c5b0a087d9cd88294
ViktorPank <41527815+ViktorPank@users.noreply.github.com>
2020-07-24 00:23:05 +0400

    add controller for FX

---
+++ javaFX_app/src/main/java/Controller/ControllerApp.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerApp implements Initializable {
+
+    @FXML
+    private Button buttonSaveResult;
+
+    @FXML
+    private Button buttonLoadResult;
+
+    @FXML
+    private Button buttonClearScreen;
+
+    @FXML
+    private Button buttonCountExample;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
