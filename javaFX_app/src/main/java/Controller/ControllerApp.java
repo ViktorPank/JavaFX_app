ddca4f83ddb9203632f9eceae83cfc037fc72dc2
ViktorPank <41527815+ViktorPank@users.noreply.github.com>
2020-07-24 00:36:23 +0400

    fix controller for FX

--- javaFX_app/src/main/java/Controller/ControllerApp.java
+++ javaFX_app/src/main/java/Controller/ControllerApp.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -21,10 +22,24 @@ public class ControllerApp implements Initializable {
     @FXML
     private Button buttonCountExample;
 
-
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void saveResult(ActionEvent actionEvent){
+
+    }
+
+    public void loadResult(ActionEvent actionEvent){
+
+    }
+
+    public void clearScreen(ActionEvent actionEvent){
+
+    }
+
+    public void countExample(ActionEvent actionEvent){
 
     }
 
